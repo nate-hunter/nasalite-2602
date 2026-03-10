1f0e5bf0259f6ea8f90a659afc1f15e421ecc580
Nate Hunter <hunter.j.nate@gmail.com>
2026-03-10 15:04:12 -0400

    Added design system related types

---
+++ src/styles/types/+variant.ts
@@ -0,0 +1,38 @@
+export type SemanticVariant =
+  | 'default'
+  | 'cta'
+  | 'primary'
+  | 'secondary'
+  | 'success'
+  | 'danger'
+  | 'warning'
+  | 'info'
+  | 'neutral';
+
+export type AppVariant = 'sunset' | 'buttery' | 'sage' | 'midnight';
+
+export type Variant = SemanticVariant | AppVariant;
+
+export type VariantBG = 'default' | 'transparent' | 'inverted';
+
+export type VariantSize = 'xxs' | 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'xxl';
+
+export type VariantMode =
+  | 'onDark'
+  | 'onDarkAlt'
+  | 'onLight'
+  | 'onLightAlt'
+  | 'on-default'
+  | 'on-alt';
+
+export type VariantShape = 'sharp' | 'straight' | 'curved' | 'rounded' | 'pill';
+
+export type VariantState =
+  | 'default'
+  | 'idle'
+  | 'loading'
+  | 'active'
+  | 'disabled'
+  | 'hover'
+  | 'focus'
+  | 'downloading';
