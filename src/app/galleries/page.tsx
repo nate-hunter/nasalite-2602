db99161c647535f00dea583930402df2d7dc80a6
Nate Hunter <hunter.j.nate@gmail.com>
2026-03-10 17:56:02 -0400

    Added initial pages for `galleries` and `wedding gallery`

---
+++ src/app/galleries/page.tsx
@@ -0,0 +1,8 @@
+export default function GalleriesPage() {
+	return (
+		<div>
+			<h2>Galleries Page...</h2>
+			<p>Add galleries here...</p>
+		</div>
+	);
+}
---
+++ src/app/galleries/wedding/page.tsx
@@ -0,0 +1,8 @@
+export default function WeddingGalleriesPage() {
+	return (
+		<div>
+			<h2>Wedding Galleries Page...</h2>
+			<p>Add wedding galleries here...</p>
+		</div>
+	);
+}
